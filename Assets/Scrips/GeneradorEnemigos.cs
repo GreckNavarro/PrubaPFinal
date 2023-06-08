60f08b22fa009a92cb16b5230248fff5e0b9efee
GreckNavarro <130480169+GreckNavarro@users.noreply.github.com>
2023-06-08 11:09:15 -0500

    GeneradorEnemigosCirculo

--- Assets/Scrips/GeneradorEnemigos.cs
+++ Assets/Scrips/GeneradorEnemigos.cs
@@ -11,7 +11,7 @@ public class GeneradorEnemigos : MonoBehaviour
     [SerializeField] PlayerControl player;
     public int numeroEnemigos;
     public int numerospawn = 10;
-    public int radio;
+    public float radio;
     public int Oleada = 1;
 
 
@@ -24,56 +24,60 @@ public class GeneradorEnemigos : MonoBehaviour
     {
         spawners = new SimpleLinkList<GameObject>();
 
-        GameObject p1 = Instantiate(lampara, (new Vector3(0, -10, 0)), transform.rotation);
+        /*GameObject p1 = Instantiate(lampara, (new Vector3(0, -10, 0)), transform.rotation);
         spawners.AddNodeAtStart(p1);
         GameObject p2 = Instantiate(lampara, (new Vector3(0, 10, 0)), transform.rotation);
         spawners.AddNodeAtStart(p2);
         GameObject p3 = Instantiate(lampara, (new Vector3(10, 0, 0)), transform.rotation);
         spawners.AddNodeAtStart(p3);
         GameObject p4 = Instantiate(lampara, (new Vector3(-10, 0, 0)), transform.rotation);
-        spawners.AddNodeAtStart(p4);
+        spawners.AddNodeAtStart(p4);*/
 
         GenerarSpawns();
 
-       StartCoroutine(GenerarEnemigos());
+        // StartCoroutine(GenerarEnemigos());
     }
 
     void Update()
     {
-        
+
     }
 
- 
+
     private void GenerarSpawns()
     {
-        float anguloasumar = ((Mathf.PI) / numerospawn);
+        float anguloasumar = (360.0f / numerospawn);
 
 
         Debug.Log(anguloasumar);
 
-       
-         for(int i = 0; i <numerospawn; i++)
-         {
-            GameObject p1 = Instantiate(lampara, (new Vector3(anguloasumar * radio, anguloasumar * radio, 0)), transform.rotation);
-            
-         }
 
+        for (int i = 0; i < numerospawn; i++)
+        {
+            float currentangulo;
+            currentangulo = (anguloasumar * i) * Mathf.Deg2Rad;
+            GameObject p1 = Instantiate(lampara, (new Vector3(Mathf.Cos(currentangulo) * radio, Mathf.Sin(currentangulo) * radio)), transform.rotation);
+            spawners.AddNodeAtStart(p1);
+        }
+
+        Debug.Log(spawners.GetCount());
     }
     private IEnumerator GenerarEnemigos()
     {
-        Debug.Log("Hola");  
-      
+
+
         for (int i = 0; i < 4; i++)
         {
             int randomposition = Random.Range(0, spawners.GetCount());
             Vector3 posicionaleatoria = spawners.GetNodeAtPosition(randomposition).transform.position;
             GameObject enemie = Instantiate(enemy, posicionaleatoria, enemy.transform.rotation);
             enemie.GetComponent<ControladorEnemy>().SetPlayer(player);
-            
+
         }
         yield return new WaitForSeconds(2f);
         StartCoroutine(GenerarEnemigos());
 
     }
+
 
 }
