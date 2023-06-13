9d1b6febbd9b088fc059d7a3b33be5bfe9ce060a
GreckNavarro <130480169+GreckNavarro@users.noreply.github.com>
2023-06-13 12:13:06 -0500

    Urp

---
+++ Assets/Scrips/UseShaderGraph.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UseShaderGraph : MonoBehaviour
+{
+    [SerializeField] List<Texture2D> textures;
+    [SerializeField] Material materialQuad;
+
+    private void Start()
+    {
+        materialQuad.SetTexture("_Texture", textures[0]);
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            materialQuad.SetTexture("_Texture", textures[1]);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        materialQuad.SetTexture("_Texture", textures[2]);
+    }
+}
