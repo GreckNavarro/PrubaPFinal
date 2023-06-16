74302393fafa82072e7c2c0396b6a5963a0bf02a
GreckNavarro <130480169+GreckNavarro@users.noreply.github.com>
2023-06-16 08:43:55 -0500

    Enemigo3

--- Assets/Scrips/ContenerEnemigos.cs
+++ Assets/Scrips/ContenerEnemigos.cs
@@ -6,14 +6,16 @@ public class ContenerEnemigos : MonoBehaviour
 {
     [SerializeField] GameObject enemy1;
     [SerializeField] GameObject enemy2;
+    [SerializeField] GameObject enemy3;
     GameObject[] enemigos;
 
 
     private void Start()
     {
-        enemigos = new GameObject[2];
+        enemigos = new GameObject[3];
         enemigos[0] = enemy1;
         enemigos[1] = enemy2;
+        enemigos[2] = enemy3;
     }
     public GameObject[] GetEnemies()
     {
